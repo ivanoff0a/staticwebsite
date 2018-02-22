6745a8630408e6a64291844063ff5dbb755b17d0
bananaWeb <a89.volkov@gmail.com>
2018-02-22 20:49:37 +0300

    v.4

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
-    
+
     $(".mobile-toggler").click(function(){
- 		
     $(".mobile-menu").toggleClass("-isOpen");
 
     var toggle = document.querySelector('.mobile-toggler');
       toggle.addEventListener('click', function(e) {
         this.classList.toggle('opened');
       });
- 	  });
+
+ 	});
+
+    setTimeout(function(){
+        $(".-from-above").addClass("-animated");
+        $(".-from-bottom").addClass("-animated");
+    }, 1000);
 
 });
