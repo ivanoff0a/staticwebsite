f3f1c642b90924d386872a4f7db53a5884e0025f
ivanoff0a <ivanov-andrey15@mail.ru>
2018-02-22 17:39:30 +0300

    v.3

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
+    
     $(".mobile-toggler").click(function(){
-        $(".mobile-menu").toggleClass("-isOpen");
-    })
+ 		
+    $(".mobile-menu").toggleClass("-isOpen");
+
+    var toggle = document.querySelector('.mobile-toggler');
+      toggle.addEventListener('click', function(e) {
+        this.classList.toggle('opened');
+      });
+ 	  });
+
 });
